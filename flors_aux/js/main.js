01d2fd644bba5c27e9712f3c60a1e2f86e08bd0d
marcroses <marcroses@gmail.com>
2014-10-03 12:29:16 +0200

    carrega2
    carrega2

---
+++ flors_aux/js/main.js
@@ -0,0 +1,55 @@
+function main() {
+    onSoc();
+	document.addEventListener("deviceready", aplicacionIniciada, false); // Al inciar la app
+	document.addEventListener("pause", aplicacionPausada, false);        // Al pausar la app
+	document.addEventListener("resume", aplicacionReiniciada, false);    // Al reiniciar la app
+	document.addEventListener("online", phonegapOnline, false);          // Phonegap tiene acceso a internet
+	document.addEventListener("offline", phonegapOffline, false);        // Phonegap NO tiene acceso a internet
+	document.addEventListener("menubutton", menuPulsado, false);         // Se ha pulsado la tecla menú
+	document.addEventListener("searchbutton", menuPulsado, false);       // Se ha pulsado la tecla búsqued
+}
+
+function aplicacionIniciada()
+{
+	FastClick.attach(document.body);
+	setTimeout(fixContentHeight,2000);
+	document.addEventListener("backbutton", atrasPulsado, false);
+}
+
+function atrasPulsado()
+{
+    if  ($.mobile.activePage.is('#mainContainer')){
+    	var msgOut="";
+    	if (currentIdioma=="ca") msgOut="Desitges sortir?";
+    	if (currentIdioma=="es") msgOut="Deseas salir?";
+    	
+	    if (confirm(msgOut) == true) {
+	        navigator.app.exitApp();
+	    }    	
+    }
+}
+ 
+function aplicacionPausada()
+{
+}
+ 
+function aplicacionReiniciada()
+{
+}
+ 
+function phonegapOnline()
+{
+}
+ 
+function phonegapOffline()
+{
+}
+ 
+function menuPulsado()
+{
+	
+}
+ 
+function busquedaPulsado()
+{
+}
