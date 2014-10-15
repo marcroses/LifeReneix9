ccceb27a428f85148f6d1e2b5a4e62cce507c531
marcroses <marcroses@gmail.com>
2014-10-15 11:48:34 +0200

    idioma 2

--- flors_aux/js/idioma.js
+++ flors_aux/js/idioma.js
@@ -6,7 +6,7 @@ function checkLocale() {
      try{
           navigator.globalization.getPreferredLanguage(
             function (locale) {
-                alert(locale.value.substr(0,2));
+                //alert(locale.value.substr(0,2));
                 currentIdioma="es";
                 if (locale.value.substr(0,2)=="ca") currentIdioma="ca";
                 setIdioma(currentIdioma);
